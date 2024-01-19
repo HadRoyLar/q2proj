16654e70505b48c75f42bf73636824b9264c91d8
HadRoyLar <hadleighroyce.lariego@smc.pshs.edu.ph>
2024-01-19 19:23:23 +0800

    Stage 1 Comp

--- src/q2proj/RegPolygon.java
+++ src/q2proj/RegPolygon.java
@@ -3,17 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package q2proj;
-
 /**
  * @author automations Yutig
  * @author polygon constructor from the internet, modified a bit  by ROyLar
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Scanner;
-
-public class RegPolygon extends JPanel {
+import javax.imageio.ImageIO;
+public class RegPolygon extends JLabel{
     public int sides;
     public int length;
     public int thick;
@@ -28,106 +30,83 @@ public class RegPolygon extends JPanel {
     public boolean mk = false;
     public int[] yPoints;
     public int[] xPoints;
-    
-    JLabel ms;
-    ImageIcon iconX = new ImageIcon(getClass().getResource("images/icon2.png"));
-    
-    
-    
-    
-    
-    
+    public void reset() {
+        s = 0;
+        xPoints = null;
+        yPoints = null;
+        val = null; 
+}
+    public BufferedImage image;
     public RegPolygon (int sides, int length, int thick, int rot) {
         this.sides = sides;
-        sides = this.sides;
         this.length = length;
         this.thick = thick;
         this.rot = rot;
-        s = sides;     
-        
-   
-    }
-    
-    
-    public void autoDraw(int side) {
-        if (s>=1) {
-            ms.setIcon(iconX);
-            ms.setVisible(true);
-        }
-        else {}
-    }
-    
+        s = sides;
+     }
     @Override
-    
     protected void paintComponent(Graphics g) {
         Graphics2D line = (Graphics2D) g;
         line.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         line.setStroke(new BasicStroke(thick));
         super.paintComponent(g);
-        drawRegularPolygon(g);     
+        drawRegularPolygon(g);   
     }
-    private void drawRegularPolygon(Graphics g) {
+    public void drawRegularPolygon(Graphics g) {
         int centerX = getWidth() / 2;
         int centerY = getHeight() / 2;
-
-        xPoints = new int[sides];
-        yPoints = new int[sides];
-        
+        xPoints = new int[s];
+        yPoints = new int[s];
         double rotx = Math.PI/rot;
-        for (int i = 0; i < sides; i++) {
-            double angle = rotx + 2 * Math.PI * i / sides;
+        for (int i = 0; i < s; i++) {
+            double angle = rotx + 2 * Math.PI * i / s;
             xPoints[i] = (int) (centerX + length * Math.sin(angle));
             yPoints[i] = (int) (centerY + length * Math.cos(angle));
-        }
-       if (num<=1) {  
-        }
-       else {
-           g.drawPolygon(xPoints, yPoints, sides);
-       }
-    }
+        } 
+        g.drawPolygon(xPoints, yPoints, s);
+}
     public String polyname() {
-        sides = s;
         polyname = "Polygon";
-        if(sides==3){
+        if(s==3){
                 polyname = "Triangle";
             }
-        else if(sides==4){
+        else if(s==4){
                 polyname = "Square";
             }
-        else if(sides==2){
+        else if(s==2){
                 polyname = "Just a Line";
             }
-        else if(sides==0){
+        else if(s==0){
                 polyname = "Nothing";
             }
-        else if(sides==1){
+        else if(s==1){
                 polyname = "Not a polygon";
             }
-            else if(sides==5){
+            else if(s==5){
                 polyname = "Pentagon";
             }
-            else if(sides==6){
+            else if(s==6){
                 polyname = "Hexagon";
             }
-            else if(sides==7){
+            else if(s==7){
                 polyname = "Heptagon";
             }
-            else if(sides==8){
+            else if(s==8){
                 polyname = "Octagon";
             }
-            else if(sides==9){
+            else if(s==9){
                 polyname = "Nonagon";
             }
-            else if(sides==10){
+            else if(s==10){
                 polyname = "Decagon";
             }
-            else if(sides==11){
+            else if(s==11){
                 polyname = "Hendecagon";
             }
-            else if(sides==12){
+            else if(s==12){
                 polyname = "Dodecagon";
             }
-            else if(sides >= 13) {
+            else if(s >= 13) {
                  polyname = (String)(sides+"-gon");   
             }
             else{
@@ -138,23 +117,19 @@ public class RegPolygon extends JPanel {
     public int[] calculate(){
         val = new int[3]; 
         System.out.println("INTERIOR AND EXTERIOR ANGLES, AND DIAGONALS OF A REGULAR POLYGON");
-        num = sides;
+        num = s;
         System.out.print("\n\nInput number of sides of the polygon:");
-        
         if (num<=2) {
             ia = 0;
             ea = 0;
             dia = 0;
         }
-        
         else if (num>=3) {
             ea = 360/num;
         ia_sum = (num-2)*180;
         ia = ia_sum/num; 
         dia = (num*(num-3))/2;
         }
-        
-        
         val[0] = (int) ia;
         val[1] = (int) ea;
         val[2] = dia;
@@ -163,33 +138,4 @@ public class RegPolygon extends JPanel {
         System.out.print("\n   d(diagonal) = " + dia );
         return val;
         }
-    public boolean draw(int ck) {
-       if (ck == 0) {
-           mk = false;
-       }
-       else if (ck == 1) {
-           mk = true;
-       }
-
-       return mk;
-        
-    }
-    
-    public void reset() {
-        
-        this.sides = 0;
-        sides = 0;
-        this.length = 0;
-        this.thick = 0;
-        this.rot = 0;
-        s = 0;
-    
-    
-    
-}
-
-    Field[] getDeclaredFields() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
 }
--- src/q2proj/automationGUI.java
+++ src/q2proj/automationGUI.java
@@ -8,7 +8,6 @@ package q2proj;
  *
  * @author RoyLar
  */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -18,14 +17,9 @@ import q2proj.RegPolygon;
 import q2proj.RoundTextField;
 import q2proj.RoundedButton;
 import java.util.Random;
-
-
 public class automationGUI implements ActionListener {
-    
-    
     RegPolygon q;
-    JPanel panel;
-    
+    JPanel panel; 
     int numsides;
     JFrame frame;
     JLabel navback;
@@ -49,17 +43,18 @@ public class automationGUI implements ActionListener {
     JLabel ean;
     JLabel intangleillus;
     JLabel extangleillus;
+    JLabel diaillus;
     JLabel polynames;
     JLabel diagonals;
     JTextField diagonalsnum;
+    JLabel intsol;
+    JLabel extsol;
+    JLabel diasol;
     String s;
     public int o;
     ImageIcon windowicon = new ImageIcon(getClass().getResource("images/icon1.png"));
-    
-    
+    ImageIcon deficon = new ImageIcon(getClass().getResource("images/icon2.png"));
     public automationGUI() {
-        
-        
         frame = new JFrame();
         frame.setIconImage(windowicon.getImage());
         navback = new JLabel();
@@ -67,32 +62,27 @@ public class automationGUI implements ActionListener {
         menu = new RoundedButton("Menu",10,220);
         check = new RoundedButton("Check",10,220);
         autotxt = new JLabel("Automations");
-        
         instruct = new JLabel("Enter the number of sides of the polygon");
         nosides = new RoundTextField(10);
         arrowto = new JLabel("└");
-        
         intangle = new RoundTextField(5);
         extangle = new RoundTextField(5);
         diagonalsnum = new RoundTextField(5);
         polynames = new JLabel("Polygon");
         ian = new JLabel("Interior Angles of Polygon");
         ean = new JLabel("Exterior Angles of Polygon");
-        diagonals = new JLabel("Number of Diagonals");
-       
-       
-        
+        diagonals = new JLabel("Number of Diagonals");     
+        intsol = new JLabel("");
+        extsol = new JLabel("");
+        diasol = new JLabel("");
 }
     public void setFrame() {
         frame.setLayout(new GraphPaperLayout(new Dimension(40, 27)));
-        
         frame.getContentPane().setBackground(new java.awt.Color(43, 43, 43, 255));
         frame.setSize(840,570);
-        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.add(autotxt, new Rectangle(33,26,8,1));
-        
         frame.add(clear, new Rectangle(1,26,4,1));
         frame.add(menu, new Rectangle(11,26,4,1));
         frame.add(check, new Rectangle(6,26,4,1));
@@ -101,120 +91,98 @@ public class automationGUI implements ActionListener {
         clear.setFont(f0);
         menu.setFont(f0);
         menu.setBackground(Color.gray);
-       
-        
-        
         frame.add(instruct, new Rectangle(2,2,20,2));
         instruct.setFont(f2);
         instruct.setForeground(Color.LIGHT_GRAY);
-        
         //EXAMPLE = new RegPolygon (No. of Sides, Side Size, LIne Width, Rotation
         //When you want the orientation to be correct No. Sides = Rotation
-        
-        
-        
-        
         frame.add(navback, new Rectangle(0,25,42,3));
         navback.setBackground(new java.awt.Color(67, 67, 67, 255));
         navback.setOpaque(true);
-        
         frame.add(nosides, new Rectangle(6,4,3,2));
         nosides.setHorizontalAlignment(JTextField.CENTER);
         nosides.setFont(f1);
         nosides.setBackground(new java.awt.Color(67, 67, 67, 255));
         nosides.setForeground(Color.LIGHT_GRAY);
-        
         intangle.setFont(f4);
         intangle.setBackground(new java.awt.Color(54, 54, 54, 255));
         intangle.setForeground(Color.LIGHT_GRAY);
         intangle.setHorizontalAlignment(JTextField.CENTER);
-        
-        
-        
         extangle.setFont(f4);
         extangle.setBackground(new java.awt.Color(54, 54, 54, 255));
         extangle.setForeground(Color.LIGHT_GRAY);
         extangle.setHorizontalAlignment(JTextField.CENTER);
-        
         diagonalsnum.setFont(f4);
         diagonalsnum.setBackground(new java.awt.Color(54, 54, 54, 255));
         diagonalsnum.setForeground(Color.LIGHT_GRAY);
         diagonalsnum.setHorizontalAlignment(JTextField.CENTER);
-        
         frame.add(intangle, new Rectangle(10,7,5,4));
         frame.add(extangle, new Rectangle(10,12,5,4));
         frame.add(diagonalsnum, new Rectangle(10,17,5,4));
-       
-        frame.add(ian, new Rectangle(2,10,8,1));
-        frame.add(ean, new Rectangle(2,15,8,1));
+        frame.add(ian, new Rectangle(2,9,8,1));
+        frame.add(ean, new Rectangle(2,14,8,1));
+        frame.add(intsol, new Rectangle(2,10,8,1));
+        frame.add(extsol, new Rectangle(2,15,8,1));
+        frame.add(diasol, new Rectangle(2,20,8,1));
+        intsol.setHorizontalAlignment(JTextField.CENTER);
+        intsol.setFont(f5);
+        extsol.setHorizontalAlignment(JTextField.CENTER);
+        extsol.setFont(f5);
+        diasol.setHorizontalAlignment(JTextField.CENTER);
+        diasol.setFont(f5);
         ian.setHorizontalAlignment(JTextField.CENTER);
         ian.setFont(f5);
-        ian.setForeground(Color.GRAY);
+        ian.setForeground(Color.LIGHT_GRAY);
         ean.setHorizontalAlignment(JTextField.CENTER);
         ean.setFont(f5);
-        ean.setForeground(Color.GRAY);
-        frame.add(diagonals, new Rectangle(2,20,8,1));
+        ean.setForeground(Color.LIGHT_GRAY);
+        frame.add(diagonals, new Rectangle(2,19,8,1));
         diagonals.setHorizontalAlignment(JTextField.CENTER);
         diagonals.setHorizontalAlignment(JTextField.CENTER);
         diagonals.setFont(f5);
-        diagonals.setForeground(Color.GRAY);
+        diagonals.setForeground(Color.LIGHT_GRAY);
         frame.add(arrowto, new Rectangle(4,4,2,2));
         arrowto.setFont(f3);
         arrowto.setBorder(new EmptyBorder(0, 0, 0, 0));
         arrowto.setForeground(Color.GRAY);
-        
         frame.add(polynames, new Rectangle(25,20,13,3));
         polynames.setFont(f1);
         polynames.setForeground(Color.LIGHT_GRAY);
         polynames.setHorizontalAlignment(JLabel.CENTER);
-        
-        
         autotxt.setForeground(Color.LIGHT_GRAY);
         autotxt.setOpaque(false);
-        autotxt.setFont(f1);
-       
+        autotxt.setFont(f1);  
         frame.setVisible(true);
-        
         clear.addActionListener(this);
         menu.addActionListener(this);
         check.addActionListener(this);
         nosides.addActionListener(this);
         
-}
-    
-   
-    public static void main(String[] args) {
-
-
-
-
+        diasol.setForeground(Color.GRAY);
+        intsol.setForeground(Color.GRAY);
+        extsol.setForeground(Color.GRAY);
         
+        
+}
+    public static void main(String[] args) {
             automationGUI q = new automationGUI();
         q.setFrame();
-  
 }
-    
-    
-
     public void actionPerformed(ActionEvent e) {
        String action = e.getActionCommand();
-       
        if(action.equals(menu.getActionCommand())) {
            frame.dispose();
            menu k = new menu();
            k.setFrame();
        }
-       
        else if(action.equals(clear.getActionCommand())) {
            intangle.setText("0°");
            extangle.setText("0°");
            diagonalsnum.setText("0");
-           
-           
-           
+           q.reset();
+           q.repaint();   
        } 
        else if(action.equals(check.getActionCommand())) {
-          
            Random r = new Random();
            char c = (char)(r.nextInt(26) + 'a');
            s = nosides.getText();
@@ -225,14 +193,10 @@ public class automationGUI implements ActionListener {
            int et = 0;
            int dg = 0;
            dat = q.calculate();
-           
-           //panel.removeAll();
-           //panel.add(q);
-           
-           frame.add(q, new Rectangle(23,3,17,17));
-        q.setOpaque(true);
+           q.setOpaque(true);
         q.setForeground(Color.LIGHT_GRAY);
         q.setBackground(new java.awt.Color(43, 43, 43, 255));
+           frame.add(q, new Rectangle(23,3,17,17));
         polynames.setText(q.polyname());
            for (int i = 0; i < dat.length; i++) {
             if (i==0) {
@@ -248,15 +212,11 @@ public class automationGUI implements ActionListener {
            intangle.setText(String.valueOf(it)+"°");
            extangle.setText(String.valueOf(et)+"°");
            diagonalsnum.setText(String.valueOf(dg)); 
-           
-           
-           
+           intsol.setText(s+"+-2)*180="+it+"°");
+           extsol.setText("360/"+s+"="+et+"°");
+           diasol.setText(s+"*("+s+"-3))/2="+dg);
        }
-       
     }
-       
-    
-
 }
        
 
