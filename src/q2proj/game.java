bc86517c0fee1df100ba5217abebc245f3b6d838
HadRoyLar <hadleighroyce.lariego@smc.pshs.edu.ph>
2024-01-20 14:52:19 +0800

    Stage 2 v2 | 2:52

--- src/q2proj/game.java
+++ src/q2proj/game.java
@@ -3,8 +3,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import javax.imageio.ImageIO;
+//* Code from WWW.tutorialsfield.com (TICTACTOE Game LOGIC)
+//* RoyLar adaptation into 'useful' GUI
 public class game implements ActionListener{
     /*IDK*/
     JFrame frame;
@@ -15,6 +19,102 @@ public class game implements ActionListener{
     JLabel gametxt;
     Font  f1  = new Font(Font.SANS_SERIF, Font.BOLD,  21);
     JButton l1, l2, l3, m1, m2, m3, r1, r2, r3;
+    
+    static String[] board;
+    static String turn;
+   
+   
+    // CheckWinner method will 
+    // decide the combination 
+    // of three box given below.
+    
+    public static String checkWinner()
+    {
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+ 
+            switch (a) {
+            case 0:
+                line = board[0] + board[1] + board[2];
+                break;
+            case 1:
+                line = board[3] + board[4] + board[5];
+                break;
+            case 2:
+                line = board[6] + board[7] + board[8];
+                break;
+            case 3:
+                line = board[0] + board[3] + board[6];
+                break;
+            case 4:
+                line = board[1] + board[4] + board[7];
+                break;
+            case 5:
+                line = board[2] + board[5] + board[8];
+                break;
+            case 6:
+                line = board[0] + board[4] + board[8];
+                break;
+            case 7:
+                line = board[2] + board[4] + board[6];
+                break;
+            }
+            //For X winner
+            if (line.equals("XXX")) {
+                return "X";
+            }
+             
+            // For O winner
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+         
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                return "draw";
+            }
+        }
+ 
+       // To enter the X Or O at the exact place on board.
+        System.out.println(
+            turn + "'s turn; enter a slot number to place "
+            + turn + " in:");
+        return null;
+    }
+     
+    // To print out the board.
+    /* |---|---|---|
+       | 1 | 2 | 3 |
+       |-----------|
+       | 4 | 5 | 6 |
+       |-----------|
+       | 7 | 8 | 9 |
+       |---|---|---|*/
+   
+    public static void printBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
+    }
+ 
+   
+    
     public game() {
         //RoundedButton Basics, example = new RoundedButton("text here", radius, shadeofgreywhenpressed)
         frame = new JFrame();
@@ -23,15 +123,15 @@ public class game implements ActionListener{
         next = new RoundedButton("→", 10, 220);
         previous = new RoundedButton("←", 10, 220);
         enter = new RoundedButton("OK", 15, 220);
-        l1 = new RoundedButton("",2,110);
-        l2 = new RoundedButton("",2,110);
-        l3 = new RoundedButton("",2,110);
-        m1 = new RoundedButton("",2,110);
-        m2 = new RoundedButton("",2,110);
-        m3 = new RoundedButton("",2,110);
-        r1 = new RoundedButton("",2,110);
-        r2 = new RoundedButton("",2,110);
-        r3 = new RoundedButton("",2,110);
+        l1 = new RoundedButton("",2,170);
+        l2 = new RoundedButton("",2,170);
+        l3 = new RoundedButton("",2,170);
+        m1 = new RoundedButton("",2,170);
+        m2 = new RoundedButton("",2,170);
+        m3 = new RoundedButton("",2,170);
+        r1 = new RoundedButton("",2,170);
+        r2 = new RoundedButton("",2,170);
+        r3 = new RoundedButton("",2,170);
        try {
     Image img = ImageIO.read(getClass().getResource(""));
     l1.setIcon(new ImageIcon(img));
@@ -81,6 +181,7 @@ public class game implements ActionListener{
         navback.setBackground(new java.awt.Color(67, 67, 67, 255));
         navback.setOpaque(true);
         frame.setVisible(true);
+        
         l1.addActionListener(this);
         l2.addActionListener(this);
         l3.addActionListener(this);
@@ -90,16 +191,100 @@ public class game implements ActionListener{
         r1.addActionListener(this);
         r2.addActionListener(this);
         r3.addActionListener(this);
+        
+        l1.setBackground(Color.GRAY);
+        l2.setBackground(Color.GRAY);
+        l3.setBackground(Color.GRAY);
+        m1.setBackground(Color.GRAY);
+        m2.setBackground(Color.GRAY);
+        m3.setBackground(Color.GRAY);
+        r1.setBackground(Color.GRAY);
+        r2.setBackground(Color.GRAY);
+        r3.setBackground(Color.GRAY);
     }
     public static void main (String[] args) {
         game q = new game();
         q.setFrame();
+        //*TIC TAC TOE LOGIC
+        
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+ 
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+ 
+        System.out.println("Welcome to 3x3 Tic Tac Toe.");
+        printBoard();
+ 
+        System.out.println(
+            "X will play first. Enter a slot number to place X in:");
+ 
+        while (winner == null) {
+            int numInput;
+           
+           // Exception handling.
+           // numInput will take input from user like from 1 to 9.
+           // If it is not in range from 1 to 9.
+           // then it will show you an error "Invalid input."
+            try {
+                numInput = in.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                        "Invalid input; re-enter slot number:");
+                    continue;
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println(
+                    "Invalid input; re-enter slot number:");
+                continue;
+            }
+             
+            // This game has two player x and O.
+            // Here is the logic to decide the turn.
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+ 
+                if (turn.equals("X")) {
+                    turn = "O";
+                }
+                else {
+                    turn = "X";
+                }
+ 
+                printBoard();
+                winner = checkWinner();
+            }
+            else {
+                System.out.println(
+                    "Slot already taken; re-enter slot number:");
+            }
+        }
+       
+        // If no one win or lose from both player x and O.
+        // then here is the logic to print "draw".
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println(
+                "It's a draw! Thanks for playing.");
+        }
+       
+        // For winner -to display Congratulations! message.
+        else {
+            System.out.println(
+                "Congratulations! " + winner
+                + "'s have won! Thanks for playing.");
+        }
+      in.close();
+ 
+      
+      //* TIC TAC TOE LOGIC END
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        
-        
         
     }
 }
